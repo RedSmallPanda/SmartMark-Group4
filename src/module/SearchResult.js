cff7c1c6c89282597587954d52e35ed6a883a43e
gyh <RemGYH@sjtu.edu.cn>
2019-04-18 15:40:03 +0800

    get from RMP

--- src/module/SearchResult.js
+++ src/module/SearchResult.js
@@ -1,38 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 import {Card, Row, Col, message, Divider} from 'antd';
 import List from "antd/es/list";
-
-const testImg = "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1555149598771&di=cc9731ab518cf536911ef138782ec329&imgtype=0&src=http%3A%2F%2Fwww.kfzimg.com%2FG06%2FM00%2FD0%2FCC%2Fp4YBAFq49ZuAGMHzAAEH07dRn8A748_b.jpg";
-const testData = [{
-    id: 1,
-    title: '数据挖掘概念与技术',
-    info: 'introduction of the book'
-}, {
-    id: 2,
-    title: '软件测试',
-    info: '一个软件工艺师的方法'
-}, {
-    id: 3,
-    title: '大学数学',
-    info: '微积分'
-}, {
-    id: 4,
-    title: '大学数学',
-    info: '微积分'
-}, {
-    id: 5,
-    title: '大学数学',
-    info: '微积分'
-}, {
-    id: 6,
-    title: '大学数学',
-    info: '微积分'
-}, {
-    id: 7,
-    title: '大学数学',
-    info: '微积分'
-},];
+import Empty from "antd/es/empty";
 
 class SearchResult extends Component {
 
@@ -41,20 +11,37 @@ class SearchResult extends Component {
         this.xmlhttp = new XMLHttpRequest();
         this.state = {
             search: window.location.search,
+            loading: true,
             mouseOver: 0,
-            data: testData,
+            data: [],
         };
-        this.postResource = this.postResource.bind(this);
+        this.getResource = this.getResource.bind(this);
         this.renderResult = this.renderResult.bind(this);
     }
 
-    postResource() {
-        // this.xmlhttp.open("POST", "http://47.103.7.215:8080/Entity/U13c635fa1f5c90/SmartMark/Sentence/", true);
-        // this.xmlhttp.setRequestHeader("Content-Type","application/json");
-        // let data = JSON.stringify({
-        //     "sentencecontent": "test value"
-        // });
-        // this.xmlhttp.send(data);
+    componentDidMount() {
+        this.getResource();
+    }
+
+    getResource() {
+        this.xmlhttp.open("GET", "http://47.103.7.215:8080/Entity/U65af91833eaa4/SmartMark3/Cover/", true);
+        this.xmlhttp.onreadystatechange = () => {
+            if (this.xmlhttp.readyState === 4 && this.xmlhttp.status === 200) {
+                let coverList = JSON.parse(this.xmlhttp.responseText);
+                if (coverList.hasOwnProperty("Cover")) {
+                    this.setState({
+                        loading: false,
+                        data: coverList["Cover"],
+                    });
+                }
+            } else if (this.xmlhttp.readyState === 4) {
+                this.setState({
+                    loading: false,
+                });
+                message.error('> BookPicker < get book Failure.', 8);
+            }
+        };
+        this.xmlhttp.send();
     }
 
     renderResult() {
@@ -64,19 +51,26 @@ class SearchResult extends Component {
                 itemLayout="horizontal"
                 dataSource={self.state.data}
                 renderItem={item => {
-                    return item.title.indexOf(decodeURI(this.state.search).substring(1, decodeURI(this.state.search).length)) === -1?
+                    let queryStr = decodeURI(this.state.search);
+                    queryStr = queryStr.substring(1, queryStr.length).toLowerCase();
+                    return item.bookid.title.toLowerCase().indexOf(queryStr) === -1 ?
                         <div/> :
                         <List.Item>
                             <List.Item.Meta
-                                avatar={<img style={{height: 220}} src={testImg}/>}
+                                avatar={<img style={{height: 220}} src={item.url}/>}
                                 title={
                                     <div style={{paddingTop: 10, paddingBottom: 10}}>
-                                        <a style={{fontSize: 25}} href={"/content/" + item.id}>{item.title}</a>
+                                        <a style={{fontSize: 25}}
+                                           href={"/content/" + item.bookid.id}>{item.bookid.title}</a>
                                     </div>
                                 }
-                                description={item.info}
+                                description={item.bookid.info}
                             />
-                            <div style={{paddingRight: 200, textAlign: 'right', verticalAlign: 'bottom'}}>Content</div>
+                            <div style={{paddingRight: 120, textAlign: 'right', verticalAlign: 'bottom', height: 0}}>
+                                <a href={"/content/" + item.bookid.id} style={{fontSize: 20}}>
+                                    前往阅读
+                                </a>
+                            </div>
                         </List.Item>;
                 }}
             />
