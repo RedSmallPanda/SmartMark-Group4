1fed720fc8db4824e9f9fd0e25f7b302ff2fbf0b
gyh <RemGYH@sjtu.edu.cn>
2019-04-20 22:03:36 +0800

    content get data

--- src/module/Content.js
+++ src/module/Content.js
@@ -1,15 +1,19 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 import {message} from 'antd';
+import moment from "moment";
+import Cookies from 'js-cookie';
 
 class Content extends Component {
 
     constructor(props) {
         super(props);
-        this.xmlhttp = new XMLHttpRequest();
         this.state = {
-            bookid: 1555590270225,
+            bookid: this.props.bookid,
+            userid: 1,
+            inputContent: '',
             content: [{content: 'test1.'}, {content: 'test2.'}],
+            marks: {},
             contentx: {
                 p0: { //第一段
                     s0: { //第一句话
@@ -37,63 +41,147 @@ class Content extends Component {
                 }
             },
         };
-        this.getResource = this.getResource.bind(this);
+        // get resources and combine
+        this.getSentences = this.getSentences.bind(this);
+        this.getMarks = this.getMarks.bind(this);
         this.sortSentence = this.sortSentence.bind(this);
+        this.combineMark = this.combineMark.bind(this);
+        // others
+    }
+
+    componentWillReceiveProps(nextProps, nextContext) {
+        message.info(JSON.stringify(nextProps), 0);
+        this.setState({...nextProps});
     }
 
     componentDidMount() {
-        this.getResource();
+        this.getSentences();
     }
 
-    sortSentence(unsorted) {
-        let sorted;
-        if (sorted.hasOwnProperty("p0")) {
-            sorted["p0"].push({content: 'test sort'});
-        } else {
-
-        }
-        sorted["p0"].push({content: 'test sort'});
-
-        return [];
-    }
-
-    getResource() {
+    getSentences() {
         if (this.state.bookid !== undefined && this.state.bookid !== null && this.state.bookid !== '') {
-            this.xmlhttp.open("GET", "http://47.103.7.215:8080/Entity/U65af91833eaa4/SmartMark3/Sentence/" +
+            let request = new XMLHttpRequest();
+            request.open("GET", "http://47.103.7.215:8080/Entity/U65af91833eaa4/SmartMark3/Sentence/" +
                 "?Sentence.bookid.id=" + this.state.bookid, true);
-            this.xmlhttp.onreadystatechange = () => {
-                if (this.xmlhttp.readyState === 4 && this.xmlhttp.status === 200) {
-                    let sentences = JSON.parse(this.xmlhttp.responseText);
+            request.onreadystatechange = () => {
+                if (request.readyState === 4 && request.status === 200) {
+                    let sentences = JSON.parse(request.responseText);
                     if (sentences.hasOwnProperty("Sentence")) {
                         let unsorted = sentences["Sentence"];
-                        this.setState({content: this.sortSentence(unsorted)});
+                        this.getMarks(unsorted);
                     }
                 }
             };
-            // this.xmlhttp.send();
+            request.send();
         }
     }
 
-    startMark(e) {
-        e.preventDefault();
+    getMarks(unsorted) {
+        if (this.state.bookid !== undefined && this.state.bookid !== null && this.state.bookid !== '') {
+            if (this.state.userid === undefined || this.state.userid === null || this.state.userid === '') {
+                return;
+            }
+            let request = new XMLHttpRequest();
+            request.open("GET", "http://47.103.7.215:8080/Entity/U65af91833eaa4/SmartMark3/Mark/" +
+                "?Mark.bookid.id=" + this.state.bookid +
+                "&Mark.userid.id=" + this.state.userid, true);
+            request.onreadystatechange = () => {
+                if (request.readyState === 4 && request.status === 200) {
+                    let marks = JSON.parse(request.responseText);
+                    let myMarks = [];
+                    if (marks.hasOwnProperty("Mark")) {
+                        myMarks = marks["Mark"];
+                    }
+                    this.sortSentence(unsorted, myMarks);
+                }
+            };
+            request.send();
+        }
     }
 
-    endMark(e) {
+    sortSentence(unsorted, myMarks) {
+        let sorted = {};
+        let marks = {};
+        unsorted.map(sentence => {
+            let paragraphProp = "p" + sentence.paragraph;
+            let sequenceProp = "s" + sentence.sequence;
+            if (!sorted.hasOwnProperty(paragraphProp)) {
+                sorted[paragraphProp] = {};
+            }
+            if (!sorted[paragraphProp].hasOwnProperty(sequenceProp)) {
+                sorted[paragraphProp][sequenceProp] = {};
+            }
+            sorted[paragraphProp][sequenceProp] = sentence;
+            sorted[paragraphProp][sequenceProp]["mark"] = [];
+        });
+        myMarks.map(mark => {
+            marks["id" + mark.id] = mark;
+            sorted = this.combineMark(mark, sorted);
+        });
+        this.setState({
+            contentx: sorted,
+            marks: marks,
+        });
+        console.log(sorted);
+        console.log(marks);
+    }
 
+    combineMark(mark, sorted) {
+        let p = mark.start.paragraph;
+        if (p === mark.end.paragraph) {
+            for (let s = mark.start.sequence; s <= mark.end.sequence; s++) {
+                sorted["p" + p]["s" + s]["mark"].push(mark);
+            }
+        } else {
+            let from = mark.start.sequence;
+            for (; p < mark.end.paragraph; p++) {
+                for (let s = from; sorted["p" + p].hasOwnProperty("s" + s); s++) {
+                    sorted["p" + p]["s" + s]["mark"].push(mark);
+                }
+                from = 0;
+            }
+            if (p === mark.end.paragraph) {
+                for (let s = 0; s <= mark.end.sequence; s++) {
+                    sorted["p" + p]["s" + s]["mark"].push(mark);
+                }
+            } else {
+                alert("combine error");
+            }
+        }
+        return sorted;
+    }
+
+    postOnClick(startId, endId) {
+        let newMark = {};
+        newMark["start"] = {id: startId};
+        newMark["end"] = {id: endId};
+        newMark["bookid"] = {id: this.state.bookid};
+        newMark["userid"] = {id: Cookies.get("userid")};
+        newMark["content"] = this.state.inputContent;
+        newMark["time"] = moment();
+
+        let newHttp = new XMLHttpRequest();
+        newHttp.open("POST", "http://47.103.7.215:8080/Entity/U65af91833eaa4/SmartMark3/Mark/", true);
+        newHttp.setRequestHeader("Content-Type", "application/json");
+        newHttp.onreadystatechange = () => {
+            if (newHttp.readyState === 4 && newHttp.status === 200) {
+                this.setState({
+                    newSentences: [-1, -1],
+                    marks: this.combineMark(newMark, this.state.contentx),
+                });
+            }
+            // 新的数据存放在newMark中，这里写setState函数，记得setState中要设置newSentences:[-1,-1]，
+            // 要完整的一条mark的话需要用返回值，newMark中关联数据只有id
+        };
+        newHttp.send(JSON.stringify(newMark));
     }
 
     render() {
         return (
             <div className="HomePage">
-                <h1>content page</h1>
-                <h2>book title</h2>
                 <div className="content">
                     {
-                        this.state.content.map(sentence =>
-                            <text className='sentence'
-                                  onMouseDown={() => message.info(sentence.content)}
-                                  onMouseUp={() => message.info(sentence.content)}>{sentence.content}</text>
-                        )
+                        JSON.stringify(this.state.contentx)
                     }
                 </div>
                 <Link to='/'>back to app</Link>
