6e3f7d3e57f9c4161910e9aa6f805cde6087ef58
gyh <RemGYH@sjtu.edu.cn>
2019-04-25 13:18:16 +0800

    add TabBar

--- SmartMarkApp/App.js
+++ SmartMarkApp/App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Icon, TabBar} from '@ant-design/react-native';
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -18,14 +19,45 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: 'homeTab',
+        };
+    }
+
+    onChangeTab(tabName) {
+        this.setState({
+            selectedTab: tabName,
+        });
+    }
+
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>Smart Mark</Text>
-                <Text style={styles.welcome}>Welcome to React Native!</Text>
-                <Text style={styles.instructions}>To get started, edit App.js</Text>
-                <Text style={styles.instructions}>{instructions}</Text>
-            </View>
+            <TabBar
+                unselectedTintColor="#949494"
+                tintColor="#33A3F4"
+                barTintColor="#f5f5f5"
+            >
+                <TabBar.Item title="Home" icon={<Icon name="home"/>}
+                             selected={this.state.selectedTab === 'homeTab'}
+                             onPress={() => this.onChangeTab('homeTab')}
+                >
+                    <View style={styles.container}>
+                        <Text style={styles.welcome}>Smart Mark APP</Text>
+                        <Text style={styles.welcome}>Welcome to React Native!</Text>
+                    </View>
+                </TabBar.Item>
+                <TabBar.Item title="Instruction" icon={<Icon name="home"/>}
+                             selected={this.state.selectedTab === 'instructionTab'}
+                             onPress={() => this.onChangeTab('instructionTab')}
+                >
+                    <View style={styles.container}>
+                        <Text style={styles.instructions}>To get started, edit App.js</Text>
+                        <Text style={styles.instructions}>{instructions}</Text>
+                    </View>
+                </TabBar.Item>
+            </TabBar>
         );
     }
 }
